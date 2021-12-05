4ef04cce367e337e0c91d16fcfc3716708396464
sidkhuntia <siddharthakhuntia@gmail.com>
2021-12-05 15:45:28 +0530

    Updates

---
+++ precache-manifest.68a44f33a75ef5e3d23ba4c2c567913e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcde2bb3fa0e96206d9ce6a8c815ae2",
+    "url": "/emoji-dict/index.html"
+  },
+  {
+    "revision": "6765ca38a5051e549036",
+    "url": "/emoji-dict/static/js/2.e3c96499.chunk.js"
+  },
+  {
+    "revision": "e9cd70034338ac79ef7c",
+    "url": "/emoji-dict/static/js/main.f233650c.chunk.js"
+  },
+  {
+    "revision": "3d3aacb5da64ae02d032",
+    "url": "/emoji-dict/static/js/runtime-main.e276447a.js"
+  }
+]);
